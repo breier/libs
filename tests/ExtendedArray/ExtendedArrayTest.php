168c8373288d084185504c704adf1f26dd6f8dc7
Andre Breier <andre@breier.net.br>
2019-10-28 21:35:32 +0000

    Increase Test Coverage

--- tests/ExtendedArray/ExtendedArrayTest.php
+++ tests/ExtendedArray/ExtendedArrayTest.php
@@ -16,8 +16,7 @@ namespace Test\ExtendedArray;
 use Breier\ExtendedArray\ExtendedArray;
 use PHPUnit\Framework\TestCase;
 
-use InvalidArgumentException;
-use TypeError;
+use Breier\ExtendedArray\ExtendedArrayException;
 
 use ArrayIterator;
 use ArrayObject;
@@ -121,38 +120,10 @@ class ExtendedArrayTest extends TestCase
         /**
          * Instantiated with SplFixedArray
          */
-        $nonAssocArray = $this->splFixedArray->toArray();
         $this->assertSame(
-            $nonAssocArray,
-            (new ExtendedArray($nonAssocArray))->getArrayCopy()
+            $this->splFixedArray->toArray(),
+            (new ExtendedArray($this->splFixedArray))->getArrayCopy()
         );
-    }
-
-    /**
-     * Test throw InvalidArgumentException Exception for non-arrays
-     *
-     * @return null
-     * @test   throw InvalidArgumentException Exception for non-arrays
-     */
-    public function throwsForNonArrays(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        $test = new ExtendedArray('not an array');
-    }
-
-    /**
-     * Test returned the correct array as JSON
-     *
-     * @return null
-     * @test   returned the correct array as JSON
-     */
-    public function returnedArrayAsJSON(): void
-    {
-        $plainArrayJSON = json_encode($this->plainArray);
-        $extendedArrayJSON = $this->extendedArray->jsonSerialize();
-
-        $this->assertSame($plainArrayJSON, $extendedArrayJSON);
     }
 
     /**
@@ -209,6 +180,227 @@ class ExtendedArrayTest extends TestCase
     }
 
     /**
+     * Test cursor moves around as expected
+     *
+     * @return null
+     * @test   cursor moves around as expected
+     */
+    public function cursorMovesAroundAsExpected(): void
+    {
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        end($this->plainArray);
+        $this->extendedArray->last();
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        reset($this->plainArray);
+        $this->extendedArray->first();
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        next($this->plainArray);
+        $this->extendedArray->next();
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        $this->assertSame(1, $this->extendedArray->pos());
+
+        prev($this->plainArray);
+        $this->extendedArray->prev();
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        prev($this->plainArray);
+        $this->extendedArray->prev();
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+
+        $this->extendedArray->seek(2);
+        $this->assertSame(
+            $this->plainArray[$this->extendedArray->key()],
+            $this->extendedArray->element()
+        );
+
+        $this->seekKey($this->plainArray, 7);
+        $this->extendedArray->seekKey(7);
+        $this->assertSame(key($this->plainArray), $this->extendedArray->key());
+    }
+
+    /**
+     * Test throw ExtendedArrayException for non-existent key
+     *
+     * @return null
+     * @test   throw ExtendedArrayException for non-existent key
+     */
+    public function throwsForNonExistentKey(): void
+    {
+        $this->expectException(ExtendedArrayException::class);
+
+        $this->extendedArray->seekKey('non-existent');
+    }
+
+    /**
+     * Test Offset Get / First / Last / Position
+     *
+     * @return null
+     * @test   Offset Get / First / Last / Position
+     */
+    public function offsetGetFirstLastPosition(): void
+    {
+        /**
+         * OffsetGet string key
+         */
+        $this->assertSame($this->plainArray['one'], $this->extendedArray->one);
+        $this->assertSame(
+            $this->plainArray['one'],
+            $this->extendedArray->offsetGet('one')
+        );
+
+        /**
+         * OffsetGet int key with ExtendedArray element
+         */
+        $this->assertSame(
+            $this->plainArray[0],
+            $this->extendedArray->{0}->getArrayCopy()
+        );
+        $this->assertSame(
+            $this->plainArray[0],
+            $this->extendedArray->offsetGet(0)->getArrayCopy()
+        );
+
+        /**
+         * OffsetGetFirst
+         */
+        $this->assertSame(
+            reset($this->plainArray),
+            $this->extendedArray->offsetGetFirst()
+        );
+
+        /**
+         * OffsetGetLast
+         */
+        $this->assertSame(
+            end($this->plainArray),
+            $this->extendedArray->offsetGetLast()
+        );
+
+        /**
+         * OffsetGetPosition
+         */
+        $this->seekKey($this->plainArray, 7); // pos 2
+        $this->assertSame(
+            current($this->plainArray),
+            $this->extendedArray->OffsetGetPosition(2)
+        );
+    }
+
+    /**
+     * Test Offset Set / Unset
+     *
+     * @return null
+     * @test   Offset Set / Unset
+     */
+    public function offsetSetUnset(): void
+    {
+        /**
+         * OffsetSet existent key (simple)
+         */
+        $this->plainArray[7] = 'seven';
+        $this->extendedArray->{7} = 'seven';
+        $this->assertSame($this->plainArray[7], $this->extendedArray->{7});
+
+        /**
+         * OffsetSet existent key (set)
+         */
+        $this->plainArray[8] = 'eight';
+        $this->extendedArray->offsetSet(8, 'eight');
+        $this->assertSame($this->plainArray[8], $this->extendedArray->offsetGet(8));
+
+        /**
+         * OffsetSet new key (simple)
+         */
+        $this->plainArray['simple'] = $this->splFixedArray;
+        $this->extendedArray->simple = $this->splFixedArray;
+        $this->assertSame(
+            $this->plainArray['simple'],
+            $this->extendedArray->simple
+        );
+        $this->assertSame(
+            array_keys($this->plainArray),
+            $this->extendedArray->keys()->getArrayCopy()
+        );
+
+        /**
+         * OffsetUnset new key (simple)
+         */
+        unset($this->plainArray['simple']);
+        unset($this->extendedArray->simple);
+        $this->assertSame(
+            array_keys($this->plainArray),
+            $this->extendedArray->keys()->getArrayCopy()
+        );
+
+        /**
+         * OffsetSet new key (set)
+         */
+        $this->plainArray['set'] = $this->splFixedArray;
+        $this->extendedArray->offsetSet('set', $this->splFixedArray);
+        $this->assertSame(
+            $this->plainArray['set'],
+            $this->extendedArray->offsetGet('set')
+        );
+        $this->assertSame(
+            array_keys($this->plainArray),
+            $this->extendedArray->keys()->getArrayCopy()
+        );
+
+        /**
+         * OffsetUnset new key (set)
+         */
+        unset($this->plainArray['set']);
+        $this->extendedArray->offsetUnset('set');
+        $this->assertSame(
+            array_keys($this->plainArray),
+            $this->extendedArray->keys()->getArrayCopy()
+        );
+    }
+
+    /**
+     * Test Array Contains element
+     *
+     * @return null
+     * @test   Array Contains element
+     */
+    public function arrayContainsElement(): void
+    {
+        $this->assertSame(
+            in_array('four', $this->plainArray),
+            $this->extendedArray->contains('four')
+        );
+
+        $byPass78756 = true; // https://bugs.php.net/bug.php?id=78756
+        $this->assertSame(
+            in_array($this->arrayObject, $this->plainArray, $byPass78756),
+            $this->extendedArray->contains($this->arrayObject)
+        );
+
+        $this->assertSame(
+            in_array($this->plainArray[0], $this->plainArray, true),
+            $this->extendedArray->contains($this->extendedArray->{0}, true)
+        );
+    }
+
+    /**
+     * Test returned the correct array as JSON
+     *
+     * @return null
+     * @test   returned the correct array as JSON
+     */
+    public function returnedArrayAsJSON(): void
+    {
+        $plainArrayJSON = json_encode($this->plainArray);
+        $extendedArrayJSON = $this->extendedArray->jsonSerialize();
+
+        $this->assertSame($plainArrayJSON, $extendedArrayJSON);
+    }
+
+    /**
      * Test Is Array works with all array types
      *
      * @return null
@@ -253,5 +445,22 @@ class ExtendedArrayTest extends TestCase
         $timeTaken = microtime(true) - $startTime;
 
         $this->assertLessThan(0.05, $timeTaken);
+    }
+
+    /**
+     * Seek Key poly-fill for extended array
+     *
+     * @param array      $array to move cursor
+     * @param int|string $key   to seek
+     *
+     * @return null
+     */
+    protected function seekKey(array &$array, $key): void
+    {
+        for (reset($array); !is_null(key($array)); next($array)) {
+            if (key($array) === $key) {
+                break;
+            }
+        }
     }
 }
