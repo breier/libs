a991032218ad7ec9b701d0e7b48bb9954685bab5
Andre Breier <andre@breier.net.br>
2020-03-22 20:39:33 +0000

    MykrORM final test coverage

--- tests/MykrORM/CRUDTest.php
+++ tests/MykrORM/CRUDTest.php
@@ -14,9 +14,9 @@
 
 namespace Test\MykrORM;
 
-use Breier\ExtendedArray\ExtendedArray;
 use PHPUnit\Framework\TestCase;
 use Test\MykrORM\MykrORMTestModel;
+use Breier\ExtendedArray\ExtendedArray;
 use Breier\MykrORM\Exception\DBException;
 
 /**
@@ -39,6 +39,9 @@ class CRUDTest extends TestCase
      */
     public function testCreateSuccess(): void
     {
+        $this->testModel->destroyDB();
+        $this->testModel = new MykrORMTestModel();
+
         $this->testModel->setText('success creation');
         $this->testModel->create();
 
@@ -49,8 +52,6 @@ class CRUDTest extends TestCase
             $newModel->getText(),
             $this->testModel->getText()
         );
-
-        $this->testModel->destroyDB();
     }
 
     /**
@@ -58,6 +59,9 @@ class CRUDTest extends TestCase
      */
     public function testCreateFailure(): void
     {
+        $this->testModel->destroyDB();
+        $this->testModel = new MykrORMTestModel();
+
         $this->testModel->setId('somethingElse');
 
         try {
@@ -70,6 +74,25 @@ class CRUDTest extends TestCase
                 $e->getMessage()
             );
         }
+    }
+
+    /**
+     * Test Find Multiple Entries
+     */
+    public function testFindMultiple(): void
+    {
+        $this->testModel->destroyDB();
+        $this->testModel = new MykrORMTestModel();
+
+        $this->testModel->setText('same text');
+        $this->testModel->create();
+        $this->testModel->create();
+
+        $newModelList = MykrORMTestModel::find(['text' => 'same text']);
+
+        $this->assertSame(2, $newModelList->count());
+        $this->assertEquals(1, $newModelList->first()->element()->getId());
+        $this->assertEquals(2, $newModelList->next()->element()->getId());
     }
 
     /**
@@ -112,11 +135,11 @@ class CRUDTest extends TestCase
     {
         return [
             'not-there' => [
-                'criteria' => ['id' => 1],
+                'criteria' => ['id' => 5],
                 'expect' => null,
             ],
             'extended-not-there' => [
-                'criteria' => new ExtendedArray(['id' => 1]),
+                'criteria' => new ExtendedArray(['id' => 5]),
                 'expect' => null,
             ],
             'empty' => [
@@ -143,5 +166,166 @@ class CRUDTest extends TestCase
                 'exec' => '$this->testModel->destroyDB();',
             ],
         ];
+    }
+
+    /**
+     * Test Update Entry in the table
+     *
+     * @dataProvider updateProvider
+     */
+    public function testUpdate($criteria, $update, $expect, $exec = null): void
+    {
+        $this->testModel->destroyDB();
+        $this->testModel = new MykrORMTestModel();
+
+        foreach ($update as $setter => $value) {
+            if (!empty($value['create'])) {
+                $value = $value['create'];
+            }
+            $this->testModel->{$setter}($value);
+        }
+        $this->testModel->create();
+
+        if (!empty($exec)) {
+            eval($exec); // dangerous uh 8)
+        }
+
+        foreach ($update as $setter => $value) {
+            if (!empty($value['update'])) {
+                $value = $value['update'];
+            }
+            $this->testModel->{$setter}($value);
+        }
+
+        if (empty($expect['exception'])) {
+            $this->testModel->update($criteria);
+
+            $newModelList = MykrORMTestModel::find($criteria);
+            $newModel = $newModelList->current();
+
+            $this->assertEquals(
+                $newModel->getProperties()->getArrayCopy(),
+                $expect
+            );
+        } else {
+            try {
+                $this->testModel->update($criteria);
+
+                $this->assertTrue(false); // Hasn't thrown an exception
+            } catch (DBException $e) {
+                $this->assertSame(
+                    $expect['exception'],
+                    $e->getMessage()
+                );
+            }
+        }
+    }
+
+    /**
+     * Update Provider
+     */
+    public function updateProvider(): array
+    {
+        return [
+            'success-one' => [
+                'criteria' => ['id' => 1],
+                'update' => [
+                    'setDate' => '2020-03-22 17:16:15',
+                ],
+                'expect' => [
+                    'id' => '1',
+                    'date' => '2020-03-22 17:16:15',
+                ],
+            ],
+            'success-two' => [
+                'criteria' => ['id' => 2],
+                'update' => [
+                    'setId' => 2,
+                    'setDate' => '2020-03-22 17:16:15',
+                ],
+                'expect' => [
+                    'id' => '2',
+                    'date' => '2020-03-22 17:16:15',
+                ],
+            ],
+            'success-three' => [
+                'criteria' => ['id' => 3],
+                'update' => [
+                    'setId' => 3,
+                    'setDate' => '2020-03-22 17:16:15',
+                    'setText' => ['create' => 'nope', 'update' => 'testing text'],
+                ],
+                'expect' => [
+                    'id' => '3',
+                    'date' => '2020-03-22 17:16:15',
+                    'text' => 'testing text',
+                ],
+            ],
+            'fail-table' => [
+                'criteria' => ['id' => 4],
+                'update' => [
+                    'setId' => 3,
+                    'setDate' => '2020-03-22 17:16:15',
+                ],
+                'expect' => ['exception' => 'Test\MykrORM\MykrORMTestModel Not Found!'],
+                'exec' => '$this->testModel->destroyDB();',
+            ],
+            'fail-criteria' => [
+                'criteria' => ['id' => 5],
+                'update' => [
+                    'setId' => 3,
+                    'setDate' => '2020-03-22 17:16:15',
+                ],
+                'expect' => ['exception' => '\'{"id":5}\' Not Found!'],
+            ],
+            'fail-data' => [
+                'criteria' => ['id' => 6],
+                'update' => [
+                    'setId' => ['create' => 6, 'update' => 'nonsense'],
+                    'setDate' => '2020-03-22 17:16:15',
+                ],
+                'expect' => [
+                    'exception' => 'SQLSTATE[HY000]: General error: 20 datatype mismatch'
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Test Delete Entry in the table
+     */
+    public function testDelete(): void
+    {
+        $this->testModel->setText('to be deleted');
+        $this->testModel->create();
+
+        try {
+            $this->testModel->delete();
+
+            $this->assertTrue(false); // Hasn't thrown an exception
+        } catch (DBException $e) {
+            $this->assertSame(
+                "'id' is empty!",
+                $e->getMessage()
+            );
+        }
+
+        $newModelList = MykrORMTestModel::find(['text' => 'to be deleted']);
+        $newModel = $newModelList->current();
+        $newModel->delete();
+
+        $newModelList = MykrORMTestModel::find(['text' => 'to be deleted']);
+        $this->assertNull($newModelList->current());
+
+        try {
+            $newModel->delete();
+
+            $this->assertTrue(false); // Hasn't thrown an exception
+        } catch (DBException $e) {
+            $this->assertSame(
+                "'id' Not Found!",
+                $e->getMessage()
+            );
+        }
     }
 }
